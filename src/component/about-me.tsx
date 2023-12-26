96ecc4198b7de73e4c66c67214811f205a3a5c23
Snap <XxSnapperGeexX@gmail.com>
2023-12-26 00:32:38 -0800

    Make About me page snapper g icon have min width.

--- src/component/about-me.tsx
+++ src/component/about-me.tsx
@@ -2,15 +2,15 @@ import SnapperGeeIcon from '../assets/snapper_g_icon.svg';
 
 const AboutMe = () =>
 (
-    <div className="sm:flex justify-center mt-24">
-        <div className="mb-4 flex-shrink-0 sm:mb-0 sm:mr-4 align-self-center">
+    <div className="flex flex-col sm:flex-row justify-center items-center mt-24">
+        <div className="mb-4 sm:mb-0 sm:mr-4">
             <img
-                className="h-32 bg-white sm:w-32"
+                className="h-32 bg-white min-w-32"
                 src={SnapperGeeIcon}
                 alt="Snapper G"
             />
         </div>
-        <div className="self-center">
+        <div className="self-center sm:text-left">
             <h4 className="text-lg font-bold">Graham Auger</h4>
             <p className="mt-1">
                 Repudiandae sint consequuntur vel. Amet ut nobis explicabo numquam expedita quia omnis voluptatem. Minus
