f409ace71b8bf129f4e5e1335b5232b3c7254e7e
Snap <XxSnapperGeexX@gmail.com>
2023-12-27 14:54:22 -0800

    Fixup indentation.

--- src/component/navbar.tsx
+++ src/component/navbar.tsx
@@ -22,24 +22,27 @@ const Navbar = () =>
                     <div className="hidden sm:ml-6 sm:block">
                         <div className="flex space-x-4">
                             {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-                            <a href="#" className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">
+                            <a
+                                href="#"
+                                className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
+                            >
                                 About me
                             </a>
                             <a
-                            href="#"
-                            className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                                href="#"
+                                className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                             >
                                 Portfolio
                             </a>
                             <a
-                            href="#"
-                            className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                                href="#"
+                                className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                             >
                                 Contact
                             </a>
                             <a
-                            href="#"
-                            className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                                href="#"
+                                className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                             >
                                 Resume
                             </a>
