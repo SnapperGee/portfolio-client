dd84c18aa0d4491dae4c8cfa2fcc8db26ba569d2
Snap <XxSnapperGeexX@gmail.com>
2023-12-28 21:40:03 -0800

    Move about me paragraphs into own component.

--- src/page/about-me.tsx
+++ src/page/about-me.tsx
@@ -3,6 +3,32 @@ import SnapperGeeIcon from "../assets/snapper_g_icon.svg";
 import { Disclosure } from "@headlessui/react";
 import { ChevronUpIcon } from "@heroicons/react/20/solid";
 
+const Paragraphs = () =>
+(
+    <>
+        <p>
+            I am proficient in utilizing build tools and package managers such as Gradle and Node/npm, and have
+            experience in implementing robust testing solutions using Junit Jupiter and Mocha with Chai testing
+            frameworks. My expertise extends to styling with frameworks like Bootstrap and Tailwind, which enables
+            me to create visually appealing and responsive user interfaces.
+        </p>
+
+        <p>
+            I have further strengthened my full-stack development capabilities by completing a comprehensive
+            Node-based course, where I received certification. This course enriched my understanding of ReactJS,
+            ExpressJS, Handlebars, Webpack, and jQuery, equipping me with the skills to build and deploy dynamic,
+            scalable applications.
+        </p>
+
+        <p>
+            With a strong foundation in both the theoretical and practical aspects of software development, I am
+            eager to bring my blend of skills and passion for coding to a professional setting, where I can
+            contribute to impactful projects and continue my journey of learning and growth in the field of software
+            development.
+        </p>
+    </>
+);
+
 const AboutMe = () =>
 (
     <div className="flex flex-col sm:flex-row justify-center items-center px-8 mt-24">
@@ -41,26 +67,7 @@ const AboutMe = () =>
                         />
                     </Disclosure.Button>
                     <Disclosure.Panel className="px-4 pb-2 pt-4 text-md text-gray-300 space-y-4" id="additionalInfo">
-                        <p>
-                            I am proficient in utilizing build tools and package managers such as Gradle and Node/npm, and have
-                            experience in implementing robust testing solutions using Junit Jupiter and Mocha with Chai testing
-                            frameworks. My expertise extends to styling with frameworks like Bootstrap and Tailwind, which enables
-                            me to create visually appealing and responsive user interfaces.
-                        </p>
-
-                        <p>
-                            I have further strengthened my full-stack development capabilities by completing a comprehensive
-                            Node-based course, where I received certification. This course enriched my understanding of ReactJS,
-                            ExpressJS, Handlebars, Webpack, and jQuery, equipping me with the skills to build and deploy dynamic,
-                            scalable applications.
-                        </p>
-
-                        <p>
-                            With a strong foundation in both the theoretical and practical aspects of software development, I am
-                            eager to bring my blend of skills and passion for coding to a professional setting, where I can
-                            contribute to impactful projects and continue my journey of learning and growth in the field of software
-                            development.
-                        </p>
+                        <Paragraphs />
                     </Disclosure.Panel>
                     </>
                 )}
