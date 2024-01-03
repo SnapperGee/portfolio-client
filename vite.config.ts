f46d5e15efd28c7107a8a0156368b71a09d725bc
Snap <XxSnapperGeexX@gmail.com>
2024-01-03 00:15:44 -0800

    Remove server proxy from vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,5 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    // server: {
-    //     proxy: {
-    //         "/": {
-    //             target: "http://localhost:3001"
-    //         },
-    //     }
-    // }
+    plugins: [react()]
 })
