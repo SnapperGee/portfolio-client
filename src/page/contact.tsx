81f65c3b5fef5210f64733f99bc423a94a3a7744
Snap <XxSnapperGeexX@gmail.com>
2024-01-05 23:15:06 -0800

    Fixup contact page indentation.

--- src/page/contact.tsx
+++ src/page/contact.tsx
@@ -353,20 +353,20 @@ const Contact = () =>
                                     type="submit"
                                     className={`inline-flex items-center gap-x-1.5 rounded-md bg-indigo-500 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500${sendingMessage ? " cursor-not-allowed": " hover:bg-indigo-400"}`} disabled={sendingMessage ? true : false}
                                 >
-                                    {
-                                        sendingMessage
-                                        ? <>
-                                            <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" fill="none" viewBox="0 0 24 24" aria-hidden="true">
-                                                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                                                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                                            </svg>
-                                            <span className="animate-pulse">Sending...</span>
-                                          </>
-                                        : <>
-                                              <EnvelopeIcon className="-ml-0.5 h-5 w-5" aria-hidden="true" />
-                                              Send message
-                                          </>
-                                    }
+                                {
+                                    sendingMessage
+                                    ? <>
+                                          <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" fill="none" viewBox="0 0 24 24" aria-hidden="true">
+                                              <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                                              <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                          </svg>
+                                          <span className="animate-pulse">Sending...</span>
+                                      </>
+                                    : <>
+                                          <EnvelopeIcon className="-ml-0.5 h-5 w-5" aria-hidden="true" />
+                                          Send message
+                                      </>
+                                }
                                 </button>
                             </div>
                         </div>
@@ -394,11 +394,11 @@ const Contact = () =>
                             <div className="p-4">
                                 <div className="flex items-start">
                                     <div className="flex-shrink-0">
-                                        {
-                                            sentMsgNotificationIcon
-                                            ? <CheckCircleIcon className="h-6 w-6 text-green-400" aria-hidden="true" />
-                                            : <XCircleIcon className="h-6 w-6 text-red-400" aria-hidden="true" />
-                                        }
+                                    {
+                                        sentMsgNotificationIcon
+                                        ? <CheckCircleIcon className="h-6 w-6 text-green-400" aria-hidden="true" />
+                                        : <XCircleIcon className="h-6 w-6 text-red-400" aria-hidden="true" />
+                                    }
                                     </div>
                                     <div className="ml-3 w-0 flex-1 pt-0.5">
                                         <p className="text-sm font-medium text-gray-900">{sentMsgNotificationHeader}</p>
@@ -409,7 +409,7 @@ const Contact = () =>
                                             type="button"
                                             className="inline-flex rounded-md text-gray-300 hover:text-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                                             onClick={() => { setShow(false) }}
-                                            >
+                                        >
                                             <span className="sr-only">Close</span>
                                             <XMarkIcon className="h-5 w-5" aria-hidden="true" />
                                         </button>
