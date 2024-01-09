5f66698b38ac865e54e16e1763d8ed53e75f69c1
Snap <XxSnapperGeexX@gmail.com>
2024-01-09 14:15:12 -0800

    Make non mobile header nav components centered.

--- src/component/header-nav.tsx
+++ src/component/header-nav.tsx
@@ -29,10 +29,10 @@ const NAV_LINKS = [
 
 const HeaderNav = () =>
 (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="bg-gray-800 sm:flex">
         {({ open }) => (
             <>
-            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div className="mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex h-16 items-center justify-between">
                     <div className="flex items-center">
                         <div className="flex-shrink-0">
