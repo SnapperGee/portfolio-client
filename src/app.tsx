a6e3f5ba5bf52badb806441d3e1c1a060c23ee29
Snap <XxSnapperGeexX@gmail.com>
2024-01-25 23:16:13 -0800

    Add doc comments to app module.

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,16 @@
+/**
+ * The root parent application component.
+ * @module app
+ */
+
 import { HeaderNav } from "./component/header-nav";
 import { FooterNav } from "./component/footer-nav";
 import { Outlet } from "react-router-dom";
 
+/**
+ * The root parent application component.
+ * @returns The root parent application component.
+ */
 export const App = () =>
 (
     <>
