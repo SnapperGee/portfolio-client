c0b575af4902e826527a62854271b60671912eba
Snap <XxSnapperGeexX@gmail.com>
2024-01-25 23:14:32 -0800

    Add doc comments and use destructured imports in router module.

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,19 @@
-import App from "./app";
-import Error from "./page/error";
-import About from "./page/about";
-import Portfolio from "./page/portfolio";
-import Contact from "./page/contact";
-import Resume from "./page/resume";
+/**
+ * This module contains the router that is used to navigate between the different webpages.
+ * @module router
+ */
+
+import { App } from "./app";
+import { Error } from "./page/error";
+import { About } from "./page/about";
+import { Portfolio } from "./page/portfolio";
+import { Contact } from "./page/contact";
+import { Resume } from "./page/resume";
 import { createBrowserRouter } from "react-router-dom";
 
+/**
+ * The router that is used to navigate between the different webpages.
+ */
 export const router = createBrowserRouter([
     {
       path: "/",
