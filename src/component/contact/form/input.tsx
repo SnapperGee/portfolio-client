88fa00e1d0bfc98aee0e79f8af56cda15084828a
Snap <XxSnapperGeexX@gmail.com>
2024-02-27 14:23:27 -0800

    Make input component style classes internal enum instead of imported.

--- src/component/contact/form/input.tsx
+++ src/component/contact/form/input.tsx
@@ -1,6 +1,11 @@
-import { InputStyleClasses } from "../../../constant/contact-form";
 import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
 import React, { useState } from "react";
+
+const enum StyleClasses
+{
+    INVALID = "ring-red-700/80 focus:ring-red-700",
+    VALID = "ring-white/10 focus:ring-indigo-500"
+}
 
 interface Props
 {
@@ -30,7 +35,7 @@ const Input: React.FC<Readonly<Props>> = ({name, predicate, invalidMessage, form
                     name={name}
                     value={inputValue}
                     onChange={(e) => setInputValue(formatter(e.currentTarget.value))}
-                    className={`block w-full rounded-md border-0 ${inputValueIsValid ? InputStyleClasses.INVALID : InputStyleClasses.VALID } bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6`}
+                    className={`block w-full rounded-md border-0 ${inputValueIsValid ? StyleClasses.INVALID : StyleClasses.VALID } bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6`}
                 />
                 <div className={`${inputValueIsValid ? "" : "hidden "}mt-2 ps-4 text-red-600`}>
                     <ExclamationTriangleIcon className="inline-block h-5" aria-hidden="true" />
