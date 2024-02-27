c48aee1861bb9e756ec51c6558bd6ce9fe1968b2
Snap <XxSnapperGeexX@gmail.com>
2024-02-27 14:29:58 -0800

    Set input ID to name in contact form input component.

--- src/component/contact/form/input.tsx
+++ src/component/contact/form/input.tsx
@@ -33,6 +33,7 @@ const Input: React.FC<Readonly<Props>> = ({name, predicate, invalidMessage, form
                 <input
                     type={type}
                     name={name}
+                    id={name}
                     value={inputValue}
                     onChange={(e) => setInputValue(format(e.currentTarget.value))}
                     className={`block w-full rounded-md border-0 ${inputValueIsValid ? StyleClasses.INVALID : StyleClasses.VALID } bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6`}
