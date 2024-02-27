d8509e3d755042af71daba50fb8137a479942326
Snap <XxSnapperGeexX@gmail.com>
2024-02-27 03:24:07 -0800

    Add additional nested div class to new contact form component.

--- src/component/contact/form/contact-form.tsx
+++ src/component/contact/form/contact-form.tsx
@@ -6,8 +6,10 @@ export default function ContactForm()
     return (
         <form className="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48" autoComplete="off">
             <div className="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
-                <NameInput />
-                <EmailInput />
+                <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2 [&>label+div]:sm:col-span-2">
+                    <NameInput />
+                    <EmailInput />
+                </div>
             </div>
         </form>
     );
