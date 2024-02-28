001567098cc40fcc3efa386ba7638e1e65157c4d
Snap <XxSnapperGeexX@gmail.com>
2024-02-27 16:45:02 -0800

    Add optional required prop to contact form input component.

--- src/component/contact/form/input.tsx
+++ src/component/contact/form/input.tsx
@@ -10,6 +10,7 @@ interface Props
     readonly format?: ((input: string) => string) | undefined;
     readonly type?: string | undefined;
     readonly label?: string | undefined;
+    readonly required?: boolean | undefined;
 }
 
 const defaultInputFormatter = (input: string) => input;
@@ -20,7 +21,8 @@ const Input: React.FC<Readonly<Props>> = ({
     invalidMessage,
     format = defaultInputFormatter,
     type = "text",
-    label = name.charAt(0).toUpperCase() + name.substring(1)
+    label = name.charAt(0).toUpperCase() + name.substring(1),
+    required = false
 }) =>
 {
     const [ inputValue, setInputValue ] = useState("");
@@ -40,6 +42,7 @@ const Input: React.FC<Readonly<Props>> = ({
                     value={inputValue}
                     onChange={(e) => setInputValue(format(e.currentTarget.value))}
                     className={`block w-full rounded-md border-0 ${inputValueIsEmpty || inputValueIsValid ? StyleClasses.VALID : StyleClasses.INVALID } bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6`}
+                    required={required}
                 />
                 <div className={`${inputValueIsEmpty || inputValueIsValid ? "hidden " : ""}mt-2 ps-4 text-red-600`}>
                     <ExclamationTriangleIcon className="inline-block size-5" aria-hidden="true" />
