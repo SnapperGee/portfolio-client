8c44d77525388ca90b51cf26387f95bff22469d0
Snap <XxSnapperGeexX@gmail.com>
2024-02-27 16:45:23 -0800

    Make MessageTextarea required.

--- src/component/contact/form/message-textarea.tsx
+++ src/component/contact/form/message-textarea.tsx
@@ -23,6 +23,7 @@ export default function MessageTextarea()
                     rows={4}
                     onChange={(e) => setInputValue(e.currentTarget.value)}
                     className={`block w-full rounded-md border-0 ${inputValueIsEmpty || inputValueIsValid ? StyleClasses.VALID : StyleClasses.INVALID } bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6`}
+                    required
                 />
                 <div className={`${inputValueIsEmpty || inputValueIsValid ? "hidden " : ""}mt-2 ps-4 text-red-600`}>
                     <ExclamationTriangleIcon className="inline-block size-5" aria-hidden="true" />
