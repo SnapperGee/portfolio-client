e1ba6403e1c14031d2b88b9c680e5a0a57194780
Snap <XxSnapperGeexX@gmail.com>
2024-03-12 13:34:57 -0700

    Revert "Remove use effect from contact form input and set state in on change handler."
    This reverts commit 2bd601dc11622c101fa91f2251cf9584136cb68c.

--- src/component/contact/form/input.tsx
+++ src/component/contact/form/input.tsx
@@ -1,6 +1,6 @@
 import { StyleClasses, type FormFieldName } from "./util";
 import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
-import React from "react";
+import React, { useEffect } from "react";
 
 interface InputProps {
     readonly name: FormFieldName;
@@ -33,13 +33,9 @@ const Input: React.FC<Readonly<InputProps>> = ({
     subLabel,
     required = false
 }) => {
-
-    const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-    {
-        const formattedValue = format(e.currentTarget.value);
-        setValue(formattedValue);
-        setValidState(formattedValue.length === 0 ? null : predicate(formattedValue));
-    };
+    useEffect(() => {
+        setValidState(value.length === 0 ? null : predicate(value));
+    }, [value, predicate, setValidState]);
 
     return (
         <>
@@ -53,7 +49,7 @@ const Input: React.FC<Readonly<InputProps>> = ({
                     name={name}
                     id={name}
                     value={value}
-                    onChange={handleOnChange}
+                    onChange={(e) => setValue(format(e.currentTarget.value))}
                     className={`block w-full rounded-md border-0 ${validState === true ? StyleClasses.VALID : validState === false ? StyleClasses.INVALID : StyleClasses.EMPTY} bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6`}
                     required={required}
                 />
