8c0220b6515d6a411276db189351a2622df60d33
Snap <XxSnapperGeexX@gmail.com>
2024-04-04 01:11:22 -0700

    Use bullet points in about page.

--- src/page/about.tsx
+++ src/page/about.tsx
@@ -12,28 +12,35 @@ import SnapperGeeIcon from "../assets/snapper_g_icon.svg";
  */
 export default function About() {
     return (
-        <div className="my-24 flex flex-col items-center justify-center px-8 sm:flex-row">
-            <div className="mb-4 sm:mb-0 sm:mr-4">
-                <img
-                    className="h-64 min-w-64 rounded-lg bg-white"
-                    src={SnapperGeeIcon}
-                    alt="Snapper G profile icon picture"
-                />
+        <>
+            <div className="my-24 flex flex-col items-center justify-center px-8 sm:flex-row">
+                <div className="mb-4 sm:mb-0 sm:mr-4">
+                    <img
+                        className="h-64 min-w-64 rounded-lg bg-white"
+                        src={SnapperGeeIcon}
+                        alt="Snapper G profile icon picture"
+                    />
+                </div>
+                <div className="self-center text-gray-300 sm:text-left">
+                    <h4 className="text-lg font-bold">Graham Auger</h4>
+                    <p className="my-1">
+                        I&apos;ve been a practicing software engineer for half a decade and have experience ranging from
+                        implementing front end client facing interfaces to back end server side logic and database
+                        administration. I also have a certificate in full stack development from the University of
+                        California Berkeley extension Computer Science training course. This has involved working in a
+                        variety of environments utilizing various languages and technologies including:
+                    </p>
+                    <ul className="list-disc ms-12">
+                        <li>npm & node</li>
+                        <li>React</li>
+                        <li>HTML, CSS/SASS, & JavaScript/TypeScript</li>
+                        <li>TailwindCSS & Bootstrap</li>
+                        <li>.NET & C<i>#</i></li>
+                        <li>Java & Gradle</li>
+                        <li>SQL & NoSQL</li>
+                    </ul>
+                </div>
             </div>
-            <div className="self-center text-gray-300 sm:text-left">
-                <h4 className="text-lg font-bold">Graham Auger</h4>
-                <p className="mt-1">
-                    I&apos;ve been a practicing software engineer for half a decade and have
-                    experience ranging from implementing front end client facing interfaces to back
-                    end server side logic and database administration. This has involved working in
-                    a variety of environments including npm and node, .NET, and Gradle. I have
-                    extensive experience with packages such as react, express, graphql, tailwind,
-                    and bootstrap. Utilizing all these technologies has involved languages such as C
-                    <i>#</i>, Java, Python, SQL, NoSQL, Typescript, Javascript, sass, css, and html.
-                    I have a certificate in full stack development from the University of California
-                    Berkeley extension Computer science training course.
-                </p>
-            </div>
-        </div>
+        </>
     );
 }
